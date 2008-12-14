5d37ce33a7d4497a5a36e8058d044598347697e8
Simon Kagstrom <simon.kagstrom@gmail.com>
2008-12-14 09:29:02 +0000

    Distutils setup stuff
    git-svn-id: http://shcov.googlecode.com/svn/trunk@20 625c770a-c827-11dd-8ca8-e3ff79f713b6

---
+++ setup.py
@@ -0,0 +1,36 @@
+######################################################################
+##
+## Copyright (C) 2008,  Simon Kagstrom
+##
+## Filename:      setup.py
+## Author:        Simon Kagstrom <simon.kagstrom@gmail.se>
+## Description:   Installation script (from Dissy)
+##
+## $Id:$
+##
+######################################################################
+import sys
+
+sys.path.append(".")
+from shcov import config
+
+from distutils.core import setup
+
+setup(name='%s' % (config.PROGRAM_NAME).lower(),
+      version='%s' % (config.PROGRAM_VERSION),
+      description="A gcov and lcov coverage test tool for bourne shell / bash scripts",
+      author="Simon Kagstrom",
+      url="%s" % (config.PROGRAM_URL),
+      author_email="simon.kagstrom@gmail.se",
+
+      packages = ['shcov'],
+      scripts = ['scripts/shcov', 'scripts/shlcov'],
+
+      data_files = [('share/%s/data' % (config.PROGRAM_NAME.lower()),
+		     ['data/amber.png', 'data/gcov.css', 'data/ruby.png',
+                      'data/emerald.png', 'data/glass.png', 'data/snow.png', ]),
+		    ('share/%s/' % (config.PROGRAM_NAME.lower()), ['menubar.xml']),
+		    ('share/doc/%s/' % (config.PROGRAM_NAME.lower()), ['README']),
+		    ('share/doc/%s/' % (config.PROGRAM_NAME.lower()), ['COPYING']),
+		    ],
+      )
---
+++ shcov/config.py
@@ -0,0 +1,5 @@
+
+PROGRAM_NAME = "shcov"
+PROGRAM_VERSION = 1
+
+PROGRAM_URL = "http://shcov.googlecode.com"
