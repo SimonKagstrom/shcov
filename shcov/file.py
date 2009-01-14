cf12b5faa0b7eb5229752b992fbe0292a1ad8949
Simon Kagstrom <simon.kagstrom@gmail.com>
2009-01-14 16:57:38 +0000

    Fixed the race condition, dereference symlinks and misc shlcov fixes
    git-svn-id: http://shcov.googlecode.com/svn/trunk@56 625c770a-c827-11dd-8ca8-e3ff79f713b6

--- shcov/file.py
+++ shcov/file.py
@@ -9,7 +9,7 @@
 ## $Id:$
 ##
 ######################################################################
-import pickle, md5, os
+import pickle, md5, os, fcntl, stat
 
 from utils import *
 
@@ -22,11 +22,19 @@ class File:
             self.source_path = source_path
 	self.basename = os.path.basename(path)
 	self.lines = {}
+	self.is_saved = False
 
 	m = md5.new()
 	m.update(read_file(self.source_path))
 
+        st = os.lstat(self.source_path)
+        self.source_file_ctime = st[stat.ST_CTIME]        
+
 	self.digest = m.digest()
+
+    def get_source_ctime(self):
+        """Return the creation time of the script"""
+        return self.source_file_ctime
 
     def set_source_path(self, path):
         self.source_path = path
@@ -34,11 +42,50 @@ class File:
     def get_source_path(self):
         return self.source_path
 
+    def has_been_saved(self):
+        return self.is_saved
+
     def save(self, path):
 	outfile = open(path, 'wb')
 
 	pickle.dump(self, outfile)
 	outfile.close()
+        self.is_saved = True
+
+    def merge_object(self, obj):
+        """Merge another object into this """
+
+        for k,v in obj.lines.items():
+            # Add the line numbering from the other
+            if self.lines.has_key(k):
+                self.lines[k] = self.lines[k] + v
+            else:
+                self.lines[k] = v
+
+
+
+    def merge_with_path(self, dst_path):
+        """Merge another object at @a dst_path into this one"""
+
+        # Create if not already there
+        fd = os.open(dst_path, os.O_CREAT | os.O_RDWR)
+        f = os.fdopen(fd, "r+")
+
+        fcntl.lockf(fd, fcntl.LOCK_EX)
+        try:
+            src = pickle.load( f )
+        except:
+            # This is OK, might not exist
+            src = File(self.path, self.source_path)
+        f.seek(0)
+
+        # If the digest matches, merge the other object into this one
+        if src.digest == self.digest:
+            self.merge_object(src)
+
+        # And save it!
+        pickle.dump(self, f)
+        f.close() # Also unlocks the file
 
     def add_to_line(self, line_nr):
 	line_nr = int(line_nr)
